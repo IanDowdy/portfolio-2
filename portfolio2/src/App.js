cc4b5a72b95aef777095f9844f838229ad60e42c
Ian <ian_dowdy@outlook.com>
2019-03-04 03:27:02 -0600

    setting up main page

--- portfolio2/src/App.js
+++ portfolio2/src/App.js
@@ -6,7 +6,7 @@ import {
   NavItem,
   NavLink,
 } from 'reactstrap';
-
+import picOfMe from './assets/pictures/me.jpg';
 import './App.css';
 
 class App extends Component {
@@ -17,19 +17,30 @@ class App extends Component {
           <NavbarBrand href="/" className="name"><h3>Ian Dowdy</h3></NavbarBrand>
           <Nav className="ml-auto" navbar>
             <NavItem>
-              <NavLink className="navlink" href="/">Projects</NavLink>
+              <NavLink className="navlink" href="/"><h2>Projects</h2></NavLink>
             </NavItem>
             <NavItem>
-              <NavLink className="navlink" href="/">Contact</NavLink>
+              <NavLink className="navlink" href="/"><h2>Contact</h2></NavLink>
             </NavItem>
           </Nav>
         </Navbar>
 
-        <div className="intro">
-          <div className="container">
+
+        <div className="container">
+          <div className="intro">
             <div className="row">
-              <div className="col-8">col-8</div>
-              <div className="col-4">col-4</div>
+              <div className="col-12"><h1>Hello! :)</h1></div>
+            </div>
+            <div className="row">
+              <div className="col-12"><br /></div>
+            </div>
+            <div className="row">
+              <div className="col-8"><span>My name is Ian, annihilator of pizza, father to the most adorable cat of all time, lover of irreverent comedies, anime, video games and Basketball (Go Spurs Go!).</span>
+                <br /><br />
+                <span>I am a passionate learner, keen on embracing this new fervor to understand as much about the world of web development as I possibly can.</span>
+                <br /><br />
+              </div>
+              <div className="col-4"><img src={picOfMe} className="img-fluid" alt="" /></div>
             </div>
           </div>
         </div>
