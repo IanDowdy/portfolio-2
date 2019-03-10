3d6f773d40d5241153863f5c0d9ceb76c4c77fb1
Ian <ian_dowdy@outlook.com>
2019-03-10 00:21:01 -0600

    Updates

---
+++ precache-manifest.abf78e610d9d21f2021593494af93efa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c08d3e85bfffc08b6de6744c94b3cd36",
+    "url": "/portfolio-2/static/media/space-jam.c08d3e85.png"
+  },
+  {
+    "revision": "96c57c5c8a49d717757eccddaa3af8be",
+    "url": "/portfolio-2/static/media/me.96c57c5c.jpg"
+  },
+  {
+    "revision": "ec9ae6bbe59f58e4cbb750f047417fdb",
+    "url": "/portfolio-2/static/media/grandmas-attic.ec9ae6bb.png"
+  },
+  {
+    "revision": "e1cfc7e4481c53795ab8",
+    "url": "/portfolio-2/static/js/runtime~main.e12e628f.js"
+  },
+  {
+    "revision": "12e70f2ccc2d5966819c",
+    "url": "/portfolio-2/static/js/main.545dec74.chunk.js"
+  },
+  {
+    "revision": "dd9bff0a82665fd046a3",
+    "url": "/portfolio-2/static/js/2.0d50baf9.chunk.js"
+  },
+  {
+    "revision": "12e70f2ccc2d5966819c",
+    "url": "/portfolio-2/static/css/main.c31c835a.chunk.css"
+  },
+  {
+    "revision": "dd9bff0a82665fd046a3",
+    "url": "/portfolio-2/static/css/2.24c4fa4d.chunk.css"
+  },
+  {
+    "revision": "1aed21e54be15b1a0c6f0c46e00c05e8",
+    "url": "/portfolio-2/index.html"
+  }
+];
