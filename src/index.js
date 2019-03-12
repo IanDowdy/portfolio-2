7ed9fec7329ad1defbfdd78e899cdbb563c0fcba
Ian <ian_dowdy@outlook.com>
2019-03-12 00:58:10 -0500

    attempting to deploy

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from './components/Home/Home';
 import Contact from './components/Contact/contact';
 import Projects from './components/Projects/projects';
@@ -14,8 +13,8 @@ ReactDOM.render(
         <div>        
         <Route path="/" component={App} />
         <Route exact path="/" component={Home} />
-        <Route path="contact" component={Contact} />
-        <Route path="projects" component={Projects} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/projects" component={Projects} />
         </div>
     </Router>
 , document.getElementById('root'));
